94dd7a4c38b3169dcbd4e2700697b115dbdc0f80
Dev Patel <dvptl68@gmail.com>
2024-04-01 20:43:42 -0500

    Test ci/cd

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Under construction, check back later...
+          Under construction...
         </p>
       </header>
     </div>
