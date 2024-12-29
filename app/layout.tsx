c3b2f5c6b64f9b6587a77ca5578632de885f4aad
Dev Patel <dvptl68@gmail.com>
2024-12-29 00:45:41 -0500

    Add global tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import './global.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
