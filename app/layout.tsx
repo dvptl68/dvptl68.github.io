aa9f6ab0fb33f92126d1f44b599e64674bc3a434
Dev Patel <dvptl68@gmail.com>
2024-12-28 22:56:51 -0500

    Reduce site to barebones and begin project structuring

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,10 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google'
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Dev M Patel",
+  title: 'Dev M Patel',
 };
 
 export default function RootLayout({
@@ -23,10 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang='en'>
+      <body className={inter.className}>
         {children}
       </body>
     </html>
---
+++ pages/HomePage.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+    return (
+      <div>
+        <main>
+          <text>Under construction...</text>
+        </main>
+      </div>
+    );
+  }
+  
